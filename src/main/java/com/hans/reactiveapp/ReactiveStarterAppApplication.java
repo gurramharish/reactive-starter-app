03d2db25a136ad121a08556a504ab27aec50e669
Harish Kumar Gurram <gurramharish12@gmail.com>
2020-08-24 15:23:32 +0530

    Initial pom setup for spring web flux apis

---
+++ src/main/java/com/hans/reactiveapp/ReactiveStarterAppApplication.java
@@ -0,0 +1,13 @@
+package com.hans.reactiveapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveStarterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveStarterAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hans/reactiveapp/ReactiveStarterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hans.reactiveapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveStarterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
